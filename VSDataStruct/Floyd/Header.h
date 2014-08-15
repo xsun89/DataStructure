bdcd6d91a79135e6ae758f1867de85d3ae533ed9
xsun89 <sunxin89@gmail.com>
2014-08-14 20:46:45 -0700

    add

---
+++ VSDataStruct/Floyd/Header.h
@@ -0,0 +1,78 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+#define OK 1
+#define ERROR 0
+#define TRUE 1
+#define FALSE 0
+#define MAXEDGE 20
+#define MAXVEX 20
+#define INFINITY 65535
+
+typedef int Status;	/* Status是函数的类型,其值是函数结果状态代码，如OK等 */
+
+typedef struct
+{
+	int vexs[MAXVEX];
+	int arc[MAXVEX][MAXVEX];
+	int numVertexes, numEdges;
+}MGraph;
+
+typedef int Patharc[MAXVEX][MAXVEX];
+typedef int ShortPathTable[MAXVEX][MAXVEX];
+
+/* 构件图 */
+void CreateMGraph(MGraph *G)
+{
+	int i, j;
+
+	/* printf("请输入边数和顶点数:"); */
+	G->numEdges = 16;
+	G->numVertexes = 9;
+
+	for (i = 0; i < G->numVertexes; i++)/* 初始化图 */
+	{
+		G->vexs[i] = i;
+	}
+
+	for (i = 0; i < G->numVertexes; i++)/* 初始化图 */
+	{
+		for (j = 0; j < G->numVertexes; j++)
+		{
+			if (i == j)
+				G->arc[i][j] = 0;
+			else
+				G->arc[i][j] = G->arc[j][i] = INFINITY;
+		}
+	}
+
+	G->arc[0][1] = 1;
+	G->arc[0][2] = 5;
+	G->arc[1][2] = 3;
+	G->arc[1][3] = 7;
+	G->arc[1][4] = 5;
+
+	G->arc[2][4] = 1;
+	G->arc[2][5] = 7;
+	G->arc[3][4] = 2;
+	G->arc[3][6] = 3;
+	G->arc[4][5] = 3;
+
+	G->arc[4][6] = 6;
+	G->arc[4][7] = 9;
+	G->arc[5][7] = 5;
+	G->arc[6][7] = 2;
+	G->arc[6][8] = 7;
+
+	G->arc[7][8] = 4;
+
+
+	for (i = 0; i < G->numVertexes; i++)
+	{
+		for (j = i; j < G->numVertexes; j++)
+		{
+			G->arc[j][i] = G->arc[i][j];
+		}
+	}
+
+}
