9172124c0b0b3d0052d69ee0e83ecad3b6e0ef3b
xsun89 <sunxin89@gmail.com>
2014-08-30 17:09:14 -0700

    add

--- VSDataStruct/AVLTree/Source.cpp
+++ VSDataStruct/AVLTree/Source.cpp
@@ -49,7 +49,101 @@ void LeftBalance(BiTree *T)
 		break;
 	case RH:
 		Lr = L->rchild;
-
+		switch (Lr->bf)
+		{
+		case LH:
+			(*T)->bf = RH;
+			L->bf = EH;
+			break;
+		case EH:
+			(*T)->bf = EH; 
+			L->bf = EH;
+			break;
+		case RH:
+			(*T)->bf = EH;
+			L->bf = LH;
+			break;
+		}
+		Lr->bf = EH;
+		L_Rotate(&(*T)->lchild);
+		R_Rotate(T);
 		break;
 	}
 }
+
+void RightBalance(BiTree *T)
+{
+	BiTree R, Rl;
+	R = (*T)->rchild; 
+	switch (R->bf)
+	{ 
+	case RH: 
+		(*T)->bf = R->bf = EH;
+		L_Rotate(T);
+		break;
+	case LH:
+		Rl = R->lchild; 
+		switch (Rl->bf)
+		{ 
+		case RH: (*T)->bf = LH;
+			R->bf = EH;
+			break;
+		case EH: (*T)->bf = R->bf = EH;
+			break;
+		case LH: (*T)->bf = EH;
+			R->bf = RH;
+			break;
+		}
+		Rl->bf = EH;
+		R_Rotate(&(*T)->rchild); 
+		L_Rotate(T); 
+	}
+}
+
+Status InsertAVL(BiTree *T, int e, Status *teller)
+{
+	if (!*T)
+	{
+		*T = (BiTree)malloc(sizeof(BiTNode));
+		(*T)->data = e;
+		(*T)->lchild = NULL;
+		(*T)->rchild = NULL;
+		(*T)->bf = EH;
+		*teller = TRUE;
+	}
+	else
+	{
+		if (e == (*T)->data)
+		{
+			*teller = FALSE;
+			return FALSE;
+		}
+
+		if (e < (*T)->data)
+		{
+			if (!InsertAVL(&(*T)->lchild, e, teller))
+			{
+				return FALSE;
+			}
+			if (*teller)
+			{
+				switch ((*T)->bf)
+				{
+				case LH:
+					LeftBalance(T);
+					*teller = FALSE;
+					break;
+				case EH:
+					(*T)->bf = LH;
+					*teller = TRUE;
+					break;
+				case RH:
+
+				}
+			}
+		}
+	}
+}
+
+
+
