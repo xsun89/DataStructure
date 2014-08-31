2ce2c372f19e6131ce6a5af775ec593cb4a13b04
xsun89 <sunxin89@gmail.com>
2014-08-30 17:24:34 -0700

    m

--- VSDataStruct/AVLTree/Source.cpp
+++ VSDataStruct/AVLTree/Source.cpp
@@ -172,5 +172,19 @@ Status InsertAVL(BiTree *T, int e, Status *teller)
 	return TRUE;
 }
 
+int main(void)
+{
+	int i;
+	int a[10] = { 3, 2, 1, 4, 5, 6, 7, 10, 9, 8 };
+	BiTree T = NULL;
+	Status taller;
+	for (i = 0; i < 10; i++)
+	{
+		InsertAVL(&T, a[i], &taller);
+	}
+	printf("set break point to see how to create AVL tree");
+	return 0;
+}
+
 
 
